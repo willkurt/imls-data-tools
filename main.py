3ad293799078f85e00b93c0787b4b4a50db53ac8
Will Kurt <wckurt@gmail.com>
2012-03-17 13:27:59 -0700

    normalized data file names for pusum and pupld

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from imlsrecord import IMLSRecord
 
 if __name__ == "__main__":
     print("sanity check for IMLSRecord")
-    f = open("./data/puout/puout2009.txt",'r')
-    testObj = IMLSRecord(f.readline(),"puout",2009)
+    fpuout = open("./data/puout/puout2009.txt",'r')
+    testObj = IMLSRecord(fpuout.readline(),"puout",2009)
     print("puout data")
     print("Print Name and address for test library")
     print(testObj.lookup("libname"))
     print(testObj.lookup("address"))
     print(testObj.lookup("city"))
     print(testObj.lookup("zip"))
-    f.close()
+    fpuout.close()
 
     print("pupld data")
     
